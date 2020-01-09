d75c99ada60ae8af097c7ec63c6cceabdac76d57
jona1993 <capitano@appl.be>
2020-01-09 13:59:31 +0100

    Implement LibreOffice converter

---
+++ RTFToPDFConvert/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using LibreOfficeLibrary;
+
+namespace RTFToPDFConvert
+{
+    class Program
+    {
+		//LibreOfficeLibrary.DocumentConverter convert = new DocumentConverter();
+		static void Main(string[] args)
+        {
+			string[] pdfFiles = Directory.GetFiles(@"C:\Users\jona1993\Documents\FR", "*")
+									 .Select(Path.GetFileName)
+									 .ToArray();
+
+			LibreOfficeConverter converter = new LibreOfficeConverter();
+
+			foreach (string f in pdfFiles)
+			{
+				converter.ConvertToPDF(f);
+			}
+			
+
+            Console.WriteLine("Hello World!");
+
+			Console.Read();
+
+			//var conv = new DocumentConverter();
+
+			//conv.ConvertToPdf(@"C:\Users\jona1993\Documents\From.rtf", @"C:\Users\jona1993\Documents\to.pdf"); // Fonctionne pas (Mais d'apres le code source, fait la même chose)
+
+		}
+
+		
+	}
+
+
+}
